eb75e9e51805261c4a6eb01948ae0e9f668f2341
Dmytro <111579384+Dmytro1117@users.noreply.github.com>
2022-11-21 08:56:15 +0200

    11

---
+++ src/axios.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+const URL = 'https://pixabay.com/api/';
+const KEY = '31420131-aff65dfb3f4bd8a8d020782c7';
+
+export default class PostsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchPost() {
+    const OPTIONS = new URLSearchParams({
+      key: KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: this.page,
+      per_page: 40,
+    });
+
+    try {
+      const response = await axios.get(`${BASE_URL}?${OPTIONS.toString()}`);
+
+      this.incrementPage();
+
+      return response.data.hits;
+    } catch (error) {
+      console.error(error.toJSON());
+    }
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Notify from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
